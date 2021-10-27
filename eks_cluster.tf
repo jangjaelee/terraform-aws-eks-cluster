6417a8978b46472b8257b1195521da135fd66161
root <root@MZC02-JJLEE.localdomain>
2021-10-27 15:04:55 +0900

    update

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -1,4 +1,6 @@
 resource "aws_eks_cluster" "this" {
+  count    = var.kms_enable ? 1 : 0
+
   name     = var.cluster_name
   version  = var.cluster_version
   role_arn = aws_iam_role.this.arn
@@ -20,8 +22,6 @@ resource "aws_eks_cluster" "this" {
   }
   
   encryption_config {
-    count = var.kms_enable ? 1 : 0
-
     provider {
       key_arn = var.kms_arn_eks
     }
@@ -36,6 +36,42 @@ resource "aws_eks_cluster" "this" {
   tags = merge(
     {
       "Name" = format("%s-eks-cluster", var.cluster_name)      
+    },
+    local.common_tags,
+  )
+}
+
+resource "aws_eks_cluster" "this1" {
+  count    = var.kms_enable ? 0 : 1
+
+  name     = var.cluster_name
+  version  = var.cluster_version
+  role_arn = aws_iam_role.this.arn
+
+  vpc_config {
+    endpoint_private_access = true
+    endpoint_public_access  = var.cluster_endpoint_public_access
+    public_access_cidrs     = var.cluster_public_access_cidrs
+    security_group_ids      = [aws_security_group.this.id]
+    subnet_ids              = data.aws_subnet_ids.cluster_subnets.ids
+  }
+
+  enabled_cluster_log_types = var.cluster_log_types
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "20m"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.this,
+    aws_cloudwatch_log_group.this
+  ]
+
+  tags = merge(
+    {
+      "Name" = format("%s-eks-cluster", var.cluster_name)
     },
     local.common_tags,
   )
