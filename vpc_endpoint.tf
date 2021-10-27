044a8e7d388338d2336999f5b34f799b2d56bdce
Alex Lee <cine0831@gmail.com>
2021-10-27 14:11:58 +0900

    update vpc_endpoint.tf

--- vpc_endpoint.tf
+++ vpc_endpoint.tf
@@ -1,9 +1,8 @@
-resource "aws_vpc_endpoint" "vpce" {
+resource "aws_vpc_endpoint" "ecr-dkr" {
   count  = var.create_endpoint_ecr ? 1 : 0
 
-  for_each = toset(["ecr.dkr", "ecr.api"])
   vpc_id = data.aws_vpc.this.id
-  service_name = "com.amazonaws.${data.aws_region.current.name}.${each.value}"
+  service_name = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
   vpc_endpoint_type = "Interface"
 
   security_group_ids = [aws_security_group.vpce.id]
@@ -12,7 +11,26 @@ resource "aws_vpc_endpoint" "vpce" {
 
   tags = merge(
     {
-      Name = "${var.cluster_name}-${each.value}-vpce"
+      Name = "${var.vpc_name}-ecr.dkr-vpce"
+    },
+    local.common_tags,
+  )
+}
+
+resource "aws_vpc_endpoint" "ecr-api" {
+  count  = var.create_endpoint_ecr ? 1 : 0
+
+  vpc_id = data.aws_vpc.this.id
+  service_name = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [aws_security_group.vpce.id]
+  subnet_ids = data.aws_subnet_ids.vpce_subnets.ids
+  private_dns_enabled = true
+
+  tags = merge(
+    {
+      Name = "${var.vpc_name}-ecr.api-vpce"
     },
     local.common_tags,
   )
